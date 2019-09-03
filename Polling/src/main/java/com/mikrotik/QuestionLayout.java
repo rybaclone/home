45f67ce6c575fc570c2d6edeecbcf971773874d0
Ryba <rybaclone@gmail.com>
2019-09-03 17:11:52 +0300

    fix

--- Polling/src/main/java/com/mikrotik/QuestionLayout.java
+++ Polling/src/main/java/com/mikrotik/QuestionLayout.java
@@ -1,21 +1,20 @@
 package com.mikrotik;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.mikrotik.bean.Question;
+import com.mikrotik.repo.AnswerRepository;
+import com.mikrotik.ui.AnswerListView;
+import com.mikrotik.ui.QuestionView;
 import com.vaadin.data.Binder;
 import com.vaadin.data.converter.LocalDateToDateConverter;
-import com.vaadin.data.converter.StringToDateConverter;
+import com.vaadin.data.converter.StringToIntegerConverter;
 import com.vaadin.data.converter.StringToLongConverter;
-import com.vaadin.icons.VaadinIcons;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.DateField;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.themes.ValoTheme;
 
@@ -23,26 +22,21 @@ public class QuestionLayout extends HorizontalLayout {
 
 	private static final long serialVersionUID = 1L;
 
-	private final CheckBox cb;
+	private final CheckBox done;
 	private final TextField name;
-	private final DateField date; 
+	private final DateField date;
 	private final TextField rating;
-
-	//TODO: NEXT
-	private final Grid<Question> grid;
 
 	public QuestionLayout(Question question, QuestionChangeListener changeListener) {
 		setWidth("100%");
 		setDefaultComponentAlignment(Alignment.MIDDLE_LEFT);
 
 		/* initiate */
-		cb = new CheckBox();
+		done = new CheckBox();
 		name = new TextField();
-		date = new DateField();
+		date = new DateField(); // NEW
 		rating = new TextField();
-		// TODO: NEXT
-		// grid = new Grid<>(Question.class);
-		
+				
 		name.setEnabled(false);
 		date.setDateFormat("dd-MMM");
 		date.setEnabled(false);
@@ -51,20 +45,20 @@ public class QuestionLayout extends HorizontalLayout {
 		rating.setWidth("50");
 
 		Binder<Question> binder = new Binder<>(Question.class);
-
+		
 		// Binds fields in this class to those in Question based on their names
 		// binder.bindInstanceFields(this);
 
 		// The following does the same more explicitly
 		binder.bind(name, Question::getName, Question::setName);
-		binder.bind(cb, Question::isDone, Question::setDone);
-		binder.forField(rating).withConverter(new StringToLongConverter("Wrong  number")).bind("rating");
-
+		binder.bind(done, Question::isDone, Question::setDone);
+		binder.forField(rating).withConverter(new StringToIntegerConverter("Wrong  number")).bind("rating");
+				 	
 		binder.forField(date).withConverter(new LocalDateToDateConverter()).bind("createDate");
 
-		binder.setBean(question);		
+		binder.setBean(question);
 
-		addComponent(cb);
+		addComponent(done);
 
 		addComponentsAndExpand(name);		
 		addComponent(date);
